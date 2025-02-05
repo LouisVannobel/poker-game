dc0cec1da83be403f914bf60ed754a2fabe636d0
LouisVannobel <louisvannob@gmail.com>
2025-02-05 17:36:05 +0100

    Initial commit

---
+++ src/player.rs
@@ -0,0 +1,24 @@
+use crate::card::Card;
+
+#[derive(Debug, Clone)]
+pub struct Player {
+    pub name: String,
+    pub chips: u32,
+    pub hand: Vec<Card>,
+    pub is_human: bool,
+    pub is_active: bool,
+    pub current_bet: u32,
+}
+
+impl Player {
+    pub fn new(name: String, is_human: bool) -> Self {
+        Self {
+            name,
+            chips: 100,
+            hand: Vec::new(),
+            is_human,
+            is_active: true,
+            current_bet: 0,
+        }
+    }
+}
